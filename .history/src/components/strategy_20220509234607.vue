f9636a20e9a942487064c3cb1383139d729229fd
Liu Ruohui <1829504353@qq.com>
2022-05-09 23:52:16 +0800

    页面初始化

--- .history/src/components/strategy_20220509234607.vue
+++ .history/src/components/strategy_20220509234607.vue
@@ -5,12 +5,12 @@
 <template>
     <div class="w-full m-4 flex flex-row bg-slate-50">
         <div class="flex h-full w-1/3 shadow-md">
-            <div class="m-4 w-full">14567</div>
+            <div class="m-4 w-full">1234567</div>
         </div>
         <div class="flex flex-grow flex-col shadow-md">
-            <div class="m-4 w-full h-1/4">123467</div>
+            <div class="m-4 w-full h-1/4">1234567</div>
             <div class="w-full flex flex-grow">
-                <div class="m-4">13</div>
+                <div class="m-4">123</div>
             </div>
         </div>
     </div>
